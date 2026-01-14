fa4f451d0e669158fb0fada960b482f842c69bdb
sujalmh9@gmail.com <sujalmh9@gmail.com>
2026-01-14 20:18:10 +0530

    initial implementation of parse and tokenization of wiki-dump

---
+++ wiki-dump/verify.py
@@ -0,0 +1,9 @@
+import bz2
+
+path = "wiki-dump\\enwiki-20260101-pages-articles-multistream.xml.bz2"
+
+with bz2.open(path, "rt", encoding="utf-8", errors="ignore") as f:
+    for i, line in enumerate(f):
+        if i > 50:
+            break
+        print(line.strip())
